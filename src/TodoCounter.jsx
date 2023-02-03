540d3a4e49005e759791d224035db9e3e509c3e2
danielbatres <danielbatresuser@gmail.com>
2023-02-03 16:01:56 -0600

    Inline styles

--- src/TodoCounter.jsx
+++ src/TodoCounter.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+/* Adding inline styles */
+const styles = {
+    color: "red",
+    backgroundColor: "yellow",
+};
+
 const TodoCounter = () => {
-    return <h2>You have completed 2 of 3 TODO's</h2>;
+    /* An object is added */
+    return <h2 style={styles}>You have completed 2 of 3 TODO's</h2>;
 }
 
 export { TodoCounter };
