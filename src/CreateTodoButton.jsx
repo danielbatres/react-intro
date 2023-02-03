7fbf0aba8994ff83f7fe218df12dff796ba155f6
danielbatres <danielbatresuser@gmail.com>
2023-02-03 16:55:13 -0600

    Anonymous function for the function that receives arguments

--- src/CreateTodoButton.jsx
+++ src/CreateTodoButton.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import "./CreateTodoButton.css";
 
 const CreateTodoButton = () => {
-  const onClikButton = () => {
-    alert("Here the modal should be shown");
+  const onClikButton = (msg) => {
+    alert(msg);
   };
 
   return (
-    /* The function is sent without parentheses to show that it is not executed but it is the function that will be used */
-    <button className="CreateTodoButton" onClick={onClikButton}>
+    /* In this case the function receives arguments so it is wrapped in an anonymous function to be used */
+    <button
+      className="CreateTodoButton"
+      onClick={() => onClikButton("Here the modal should be shown")}
+    >
       +
     </button>
   );
