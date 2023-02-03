ef4399a72631dc2ca7a43e1e30e6f5cd2921023c
danielbatres <danielbatresuser@gmail.com>
2023-02-03 16:35:38 -0600

    Added completed property

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { CreateTodoButton } from "./CreateTodoButton";
 const todos = [
   { text: "Cut onion", completed: false }, 
   { text: "Take react intro course", completed: false }, 
-  { text: "Do homework", completed: false }
+  { text: "Do homework", completed: true }
 ];
 
 function App() {
@@ -19,7 +19,11 @@ function App() {
       <TodoList>
         {todos.map(todo => (
           /* A unique key is added to identify the components within a list */
-          <TodoItem key={todo.text} text={todo.text} />
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text} 
+            completed={todo.completed}
+          />
         ))}
       </TodoList>
       <CreateTodoButton />
