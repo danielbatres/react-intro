756f5a71d34c299708019627fc576d9fc313db3f
danielbatres <danielbatresuser@gmail.com>
2023-02-04 14:30:52 -0600

    useState handling for TodoSearch

--- src/TodoSearch.jsx
+++ src/TodoSearch.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React from "react";
 import "./TodoSearch.css";
 
-const TodoSearch = () =>{
-    /**
-     * useState returns a two positions array.
-     * The state variable and the function to change that state.
-     * The initial state is received as an argument.
-     * 
-     * Every time the state changes the component is rendered again.
-     */ 
-    const [state, setState] = React.useState('empty');
+const TodoSearch = () => {
+  /**
+   * useState returns a two positions array.
+   * The state variable and the function to change that state.
+   * The initial state is received as an argument.
+   *
+   * Every time the state changes the component is rendered again.
+   */
+  const [searchValue, setSearchValue] = React.useState("");
 
-    const onSearchValueChange = (event) => {
-        setState(event.target.value);
-    }
-    
-    return (
-        <>
-            <input className='TodoSearch' placeholder="Onion" onChange={onSearchValueChange}/>
-            <p>{state}</p>
-        </>
-    );
-}
+  const onSearchValueChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  return (
+    <>
+      <input
+        className="TodoSearch"
+        placeholder="Onion"
+        onChange={onSearchValueChange}
+      />
+      <p>{searchValue}</p>
+    </>
+  );
+};
 
 export { TodoSearch };
