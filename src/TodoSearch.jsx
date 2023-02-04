e8f172346a1df27623bcebc0a7facd9bd1ec6e63
danielbatres <danielbatresuser@gmail.com>
2023-02-04 14:34:52 -0600

    Props destructuring

--- src/TodoSearch.jsx
+++ src/TodoSearch.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import "./TodoSearch.css";
 
-const TodoSearch = () => {
+/**
+ * We destructure the props object to receive only
+ * it's properties by parameters
+ */
+const TodoSearch = ({ searchValue, setSearchValue }) => {
   /**
    * useState returns a two positions array.
    * The state variable and the function to change that state.
    * The initial state is received as an argument.
    *
    * Every time the state changes the component is rendered again.
+   *
+   * const [searchValue, setSearchValue] = React.useState('');
    */
-  const [searchValue, setSearchValue] = React.useState("");
 
   const onSearchValueChange = (event) => {
     setSearchValue(event.target.value);
