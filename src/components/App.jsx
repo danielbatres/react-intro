b4781697300692aa619ca9c5002e9b31bf5c67da
danielbatres <danielbatresuser@gmail.com>
2023-02-06 19:03:31 -0600

    Add hooks folder and file for useLocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,12 @@
 import React from 'react';
 import { AppUI } from './AppUI';
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 //const defaultTodos = [
 //  { text: "Cut onion", completed: false },
 //  { text: "Take react intro course", completed: false },
 //  { text: "Do homework", completed: true }
 //];
-
-function useLocalStorage(itemName, initialValue) {
-  /**
-  * Within the custom hooks we can call the official React hooks
-  */
-  const [error, setError] = React.useState(false);
-  const [loading, setLoading] = React.useState(true);
-  const [item, setItem] = React.useState(initialValue);
-
-  React.useEffect(() => {
-    setTimeout(() => {
-      try {
-        const localStorageItem = localStorage.getItem(itemName);
-        let parsedItem;
-
-        if (!localStorageItem) {
-          localStorage.setItem(itemName, JSON.stringify(initialValue));
-          parsedItem = [];
-        } else {
-          parsedItem = JSON.parse(localStorageItem);
-        }
-
-        setItem(parsedItem);
-        setLoading(false);
-      } catch (error) {
-        setError(error);
-      }
-      }, 1000);
-  });
-
-  const saveItem = newItem => {
-    try {
-      const stringifiedItem = JSON.stringify(newItem);
-      localStorage.setItem(itemName, stringifiedItem);
-      setItem(newItem);
-    } catch (error) {
-      setError(error);
-    }
-  }
-
-  /**
-   * When a custom hook returns several values it is recommended to use an object
-   * for it's return.
-   */
-  return {
-    item,
-    saveItem,
-    loading,
-    error
-  };
-}
 
 function App() {
   /**
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+function useLocalStorage(itemName, initialValue) {
+  /**
+  * Within the custom hooks we can call the official React hooks
+  */
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const [item, setItem] = React.useState(initialValue);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = [];
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
+
+        setItem(parsedItem);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+      }
+    }, 1000);
+  });
+
+  const saveItem = newItem => {
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch (error) {
+      setError(error);
+    }
+  }
+
+  /**
+   * When a custom hook returns several values it is recommended to use an object
+   * for it's return.
+   */
+  return {
+    item,
+    saveItem,
+    loading,
+    error
+  };
+}
+
+export { useLocalStorage };
