c9a63042bc8d9e675df6217b5f35f3d0507857ce
danielbatres <danielbatresuser@gmail.com>
2023-02-08 13:13:07 -0600

    CSS file for modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../styles/Modal.css';
 
 function Modal({ children }) {
   return ReactDOM.createPortal(
-    children,
+    <div className="ModalBackground">{children}</div>,
     document.getElementById('modal')
   );
 }
