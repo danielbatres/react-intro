19a394b927eba691f19c4eed7d2e22ea76d5f7e9
danielbatres <danielbatresuser@gmail.com>
2023-02-08 13:42:13 -0600

    Modal updater with CreateTodoButton

--- src/components/AppUI.jsx
+++ src/components/AppUI.jsx
@@ -43,7 +43,7 @@ function AppUI() {
           <p>TELEPORTATION</p>
         </Modal>
       )}
-      <CreateTodoButton />
+      <CreateTodoButton setOpenModal={setOpenModal}/>
     </>
   );
 }
--- src/components/CreateTodoButton.jsx
+++ src/components/CreateTodoButton.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "../styles/CreateTodoButton.css";
 
-const CreateTodoButton = () => {
-  const onClikButton = (msg) => {
-    alert(msg);
+const CreateTodoButton = ({ setOpenModal }) => {
+  const onClikButton = () => {
+    setOpenModal(true);
   };
 
   return (
     /* In this case the function receives arguments so it is wrapped in an anonymous function to be used */
     <button
       className="CreateTodoButton"
-      onClick={() => onClikButton("Here the modal should be shown")}
+      onClick={onClikButton}
     >
       +
     </button>
