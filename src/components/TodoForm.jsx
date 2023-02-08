8fb2701c0c12f6eb16ca1d23c7b85a08a57a1637
danielbatres <danielbatresuser@gmail.com>
2023-02-08 14:06:24 -0600

    Code and structure for TodoForm

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 
 const TodoForm = () => {
+  const onCancel = () => {
+
+  }
+
+  const onAdd = () => {
+
+  }
+  
+  const onSubmit = () => {
+    
+  }
+
   return (
-    <form></form>      
+    <form onSubmit={onSubmit}>
+      <label>...</label>
+      <textarea placeholder="Cut onion for lunch"/>
+      <div>
+        <button
+          type="button"
+          onClick={onCancel}
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          onClick={onAdd}
+          >
+          Add
+        </button>
+      </div>
+    </form>
   );
 }
 
