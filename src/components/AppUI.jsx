20897f7eebcacbdb2804a90e2fd5503c69c4f6c5
danielbatres <danielbatresuser@gmail.com>
2023-02-08 14:00:49 -0600

    TodoForm integration in the application interface

--- src/components/AppUI.jsx
+++ src/components/AppUI.jsx
@@ -6,6 +6,7 @@ import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 import { Modal } from './Modal';
+import { TodoForm } from "./TodoForm";
 
 function AppUI() {
   const {
@@ -40,7 +41,7 @@ function AppUI() {
       </TodoList>
       {openModal && (
         <Modal>
-          <p>TELEPORTATION</p>
+          <TodoForm />
         </Modal>
       )}
       <CreateTodoButton setOpenModal={setOpenModal}/>
