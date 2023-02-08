afbd9d34e817c93f5acd0732971a6490c762293f
danielbatres <danielbatresuser@gmail.com>
2023-02-07 19:51:49 -0600

    Consumer use for application interface

--- src/components/AppUI.jsx
+++ src/components/AppUI.jsx
@@ -1,49 +1,44 @@
 import React from 'react';
+import { TodoContext } from "./TodoContext";
 import { TodoCounter } from "./TodoCounter";
 import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 
-function AppUI({
-  loading,
-  error,
-  totalTodos,
-  completedTodos,
-  searchValue,
-  setSearchValue,
-  searchedTodos,
-  completeTodo,
-  deleteTodo
-}) {
+function AppUI() {
   return (
     <>
-      <TodoCounter
-        total={totalTodos}
-        completed={completedTodos}
-      />
-      <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-      <TodoList>
-        {error && <p>An error has ocurred...</p>}
-        {loading && <p>Loading, please wait...</p>}
-        {(!loading && !searchedTodos.length) && <p>¡¡Create your first TODO!!</p>}
+      <TodoCounter />
+      <TodoSearch />
+      <TodoContext.Consumer>
+        {({
+          error,
+          loading,
+          searchedTodos,
+          completeTodo,
+          deleteTodo
+        }) => (
+          <TodoList>
+            {error && <p>An error has ocurred...</p>}
+            {loading && <p>Loading, please wait...</p>}
+            {(!loading && !searchedTodos.length) && <p>¡¡Create your first TODO!!</p>}
 
-        {searchedTodos.map(todo => (
-                /* A unique key is added to identify the components within a list */
-              <TodoItem
-                key={todo.text}
-                text={todo.text}
-                completed={todo.completed}
-                onComplete={() => completeTodo(todo.text)}
-                onDelete={() => deleteTodo(todo.text)}
-              />
-              ))}
-      </TodoList>
+            {searchedTodos.map(todo => (
+                    /* A unique key is added to identify the components within a list */
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+          </TodoList>
+          )}
+      </TodoContext.Consumer>
       <CreateTodoButton />
-      </>
+    </>
   );
 }
 
